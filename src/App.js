cf72f85204d6bfd3723c9092a614b7323213d460
MansisSonwane <mansisonawane14@gmail.com>
2023-04-18 12:48:07 +0530

    add aboutus

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Aboutuspage from './Components/Aboutuspage';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Aboutuspage/>
+    </>
+  );
+}
+
+export default App;
