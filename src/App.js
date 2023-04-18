e739fcafcde1f2dc534e77c4c82a8a86c611fd1f
MansisSonwane <mansisonawane14@gmail.com>
2023-04-18 12:52:52 +0530

    removed header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Aboutuspage from './Components/Aboutuspage';
-import Header from './Components/Header';
+
 
 function App() {
   return (
     <>
-    <Header/>
+    
     <Aboutuspage/>
     </>
   );
